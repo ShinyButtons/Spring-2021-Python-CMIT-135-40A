666f68133870f8631d659ae3ee6396af0716fecb
Jennifer Roy <77632985+jennyfaraday@users.noreply.github.com>
2021-01-29 22:49:43 -0600

    Update p1_vote.py

--- Week_3/p1_vote.py
+++ Week_3/p1_vote.py
@@ -2,7 +2,7 @@
 CMIT-135-40A, Week 3: Assignment: Conditional Logic Programs, Program #1
 by Jennifer C. Roy 01/29/2021
 
-Voters rights are established by the United State Constitution and by state law. All whom are eligible, go vote.
+Voters rights are established by the United States Constitution and by state law. All whom are eligible, go vote.
 """
 age = int(input("Question: Are you eligible to vote? Please, enter your age:"))
 if age >= 18:
