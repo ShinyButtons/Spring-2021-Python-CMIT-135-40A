178b5fa57f3e60eb7c000f0f18a0665f132aa7af
Jennifer Roy <77632985+ShinyButtons@users.noreply.github.com>
2021-02-07 21:36:36 -0600

    Add files via upload

---
+++ Week_4/part2.py
@@ -0,0 +1,11 @@
+"""
+CMIT-135-40A, Week 4: Assignment: Programming Assignment - Loops, Part #2
+by Jennifer C. Roy 01/31/2021
+Write a program that prints 2^n
+where n goes from 1 to 100. Print one answer per line (note: you are answering the question,
+how high can you count on 100 fingers). Write your program using a 'for' loop.  Reminder '**' is the operator
+for exponents, so for example 4**3 is 4 to the third power, or 4 * 4 * 4 which equals 64.
+"""
+# Part 2 of assignment including N^2 with counting on 100 fingers
+for n in range(0, 101):
+    print(2 ** n)
---
+++ Week_4/part3.py
@@ -0,0 +1,27 @@
+"""
+CMIT-135-40A, Week 4: Assignment: Programming Assignment - Loops, Part#3
+by Jennifer C. Roy 01/31/2021
+Extend your guessing game from last week.  Write a program that picks a random number from 1-100.
+Then ask the user to guess a number. Tell the user if the answer is higher or lower than the number
+they guessed, or if they got the correct answer.  Allow them to guess again if they got the guess incorrect.
+They should be able to guess numbers an infinite number of times until they get the correct answer, at which
+point your loop will end.
+To generate a number from 1-100 you will need the following code at the beginning of your program:
+
+from random import randint
+randomNum = randint(1,100)
+"""
+
+from random import randint
+
+randomNum = randint(1, 100)
+
+while True:
+    guess = int(input("Guess a number I am thinking of from 1-100"))
+    if guess == randomNum:
+        print("Ding, Ding, you are a winner!")
+        break
+    elif guess < randomNum:
+        print("Your guess is less than the correct answer. Try again.")
+    else:
+        print("Your guess is higher than the correct answer. Try again.")
